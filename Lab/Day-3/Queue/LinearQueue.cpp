c6b1c42e647f770de85e4166afbddb425c5d2261
Shivam Karn <shivamkarn767@gmail.com>
2026-06-05 08:46:42 +0545

    Linear and Circular Queue in C++

---
+++ Lab/Day-3/Queue/LinearQueue.cpp
@@ -0,0 +1,67 @@
+// Linear QUEUE 
+#include <iostream>
+#include <stdexcept>
+#include <array>
+#include <utility>
+
+template <typename T, size_t Capacity>
+class LinearQueue {
+private:
+    std::array<T, Capacity> m_data{};
+    size_t m_front{0};
+    size_t m_rear{0};
+    size_t m_size{0};
+public:
+    bool isEmpty() const noexcept {
+      return m_size == 0;
+    }
+
+    bool isFull() const noexcept {
+      return m_rear == Capacity;
+    }
+
+    void enqueue(T element) {
+        if (isFull()) {
+          throw std::overflow_error("Queue is Full");
+        }
+        m_data[m_rear] = std::move(element);
+        std::cout << m_data[m_rear] << " Enqueued to Queue.\n";
+        ++m_rear;
+        ++m_size;
+    }
+
+    T dequeue() {
+        if (isEmpty()) {
+          throw std::underflow_error("Queue is Empty!");
+        }
+        T element = std::move(m_data[m_front]);
+        std::cout << "Element " << element << " removed from Queue.\n";
+        ++m_front;
+        --m_size;
+        return element;
+    }
+};
+
+int main() {
+    try {
+        LinearQueue<int, 5> que;
+
+        que.enqueue(11);
+        que.enqueue(22);
+        que.enqueue(33);
+        que.enqueue(44);
+        que.enqueue(55);
+
+        que.dequeue();
+        que.dequeue();
+
+        // In a Linear Queue, this will eventually throw 
+        // because m_rear reached the end, even if there is space at the front
+        que.enqueue(66); 
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+    }
+
+    return 0;
+}

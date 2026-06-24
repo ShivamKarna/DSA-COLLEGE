6c29b77830c2b4ffb6cc148975ff32aba59fb442
Shivam Karn <shivamkarn767@gmail.com>
2026-06-24 15:58:57 +0545

    main file

---
+++ Lab/Day-4/Sorting/main.cc
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <iostream>
+
+#include "helper.h"
+#include "selection_sort.h"
+// #include "bubble_sort.h"
+// #include "insertion_sort.h"
+
+int arr[MAX];
+
+int main() {
+  size_t n;
+
+  std::cout << "Enter n : ";
+  std::cin >> n;
+
+  if (n > MAX) {
+    std::cerr << "Invalid Size\n";
+    return 1;
+  }
+
+  generate_random_elements(arr, n);
+
+  std::cout << "Sorting...\n";
+  auto start = std::chrono::steady_clock::now();
+
+  selection_sort(arr, n);
+
+  auto end = std::chrono::steady_clock::now();
+
+  print_array(arr, n);
+
+  auto time_taken =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+  std::cout << "Time Taken is " << time_taken.count() * 1e-9 << " s\n";
+
+  return 0;
+}
